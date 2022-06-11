1a34beeb0708b7d07b01b8f7eaaa599048c2b8ee
StatisticallySignificant <mlpro0001@gmail.com>
2022-06-11 22:31:00 +0200

    Removed useless functions, improved code readability
    Some functions and var were useless so I removed them
    The comments were sometimes useless and/or in french. They are now all in english

--- Panier.py
+++ Panier.py
@@ -1,18 +1,13 @@
- # On veut qu'un panier puisse collecter les fruits
- # On peut déplacer le panier de gauche à droite et de haut en bas
- # Le fruit et le panier doivent avoir un collider
+ # We want a basket to collect the fruit
+ # The player can move the basket
+ # The basket has a collider
 import pygame
-
-# Panier and Fruit classes both has:
-# a rect, an image, a position, a direction, a speed, a draw function and a move function
-# Maybe they could have been the same class or they could have inherited from the same class
-# so we don't write two times the same code
 
 class Panier:
 	def __init__(self, x, y, speed, dimensions, direction=[0,0]):
-		self.image = pygame.image.load("panier_fondnoir_rogne.png") # It was working for you ? It couldn't find the png file for me
+		self.image = pygame.image.load("panier_fondnoir_rogne.png") 
 		self.image = pygame.transform.scale(self.image, dimensions)
-		# la direction dépend de la touche appuyée par le joueur
+		# The initial direction is (0,0)
 		self.direction = direction
 		self.speed = speed
 		self.rect = self.image.get_rect(x=x,y=y)
@@ -21,6 +16,6 @@ class Panier:
 		screen.blit(self.image, self.rect)
 
 	def move(self,delta_time):
-		# normalize vector
+		# We don't normalize vector because it can move only honrizontally
 		self.rect.move_ip(self.direction[0] * self.speed*delta_time, self.direction[1] * self.speed*delta_time)
 	
